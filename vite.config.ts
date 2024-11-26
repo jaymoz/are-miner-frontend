84bbd78b039e3a4ac06f9e04dc9cf9be2eb0e2bf
Jacob Idoko <jacob.idoko@ucalgary.ca>
2024-11-26 12:54:03 -0700

    Fixed issue with wrong api endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 8000,
-    https: {
-      key: './cert/key.pem',
-      cert: './cert/cert.pem',
-    },
-    proxy: {
+    proxy:{
       '/api': {
-        target: 'https://52.91.210.118:443',
+        target: 'http://52.91.210.118:80',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,  // For self-signed certificates
-        ssl: {
-          key: './cert/key.pem',
-          cert: './cert/cert.pem',
-        }
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
 })
+
